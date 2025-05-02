8f56e5fe3c1cce565582ff5c834d7ad4a5dadfb1
Jam516 <kufuorkofi@gmail.com>
2025-05-02 13:59:26 +0100

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export function SiteFooter() {
     return (
         <footer className="border-t border-gray-300">
-            <div className="flex flex-col mt-3">
+            <div className="flex flex-row justify-between mt-3">
                 <p className="text-center text-sm text-muted-foreground">
                     Built by{" "}
                     <a
@@ -23,6 +23,30 @@ export function SiteFooter() {
                     </a>
                     .
                 </p>
+                <div className=" flex flex-row gap-3 text-center text-sm text-muted-foreground">
+                    Sponsored by{" "}
+                    <a
+                        href="https://www.pimlico.io/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="/pimlico-logo.png" className="h-5" alt="Pimlico" />
+                    </a>
+                    <a
+                        href="https://www.biconomy.io/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="/biconomy-logo.png" className="h-5" alt="Biconomy" />
+                    </a>
+                    {/* <a
+                        href="https://twitter.com/0xKofi"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="/ethereum-foundation-logo.png" className="h-5" alt="BundleBear" />
+                    </a> */}
+                </div>
             </div>
         </footer>
     );
