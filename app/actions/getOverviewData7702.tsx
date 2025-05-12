ef77f281efb9a2c4107234c3d80fa6de1e03d1c1
Jam516 <kufuorkofi@gmail.com>
2025-05-12 08:03:02 +0100

    commit

--- app/actions/getOverviewData7702.tsx
+++ app/actions/getOverviewData7702.tsx
@@ -1,4 +1,4 @@
-// import { unstable_noStore as noStore } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 
 interface OverviewDataParams {
     chain: string;
@@ -16,7 +16,7 @@ interface ChainData {
 }
 
 export async function getOverviewData7702({ chain, timeframe }: OverviewDataParams): Promise<ChainData> {
-    // noStore();
+    noStore();
     const response = await fetch(`https://bundlebear-api.onrender.com/eip7702-overview?chain=${chain}&timeframe=${timeframe}`);
     // , { next: { revalidate: 3600 } }
 
