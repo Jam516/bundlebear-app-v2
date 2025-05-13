ef713ce2e6a8210e150270d5e0a6472c19b2be4f
Jam516 <kufuorkofi@gmail.com>
2025-05-13 18:40:06 +0100

    remove label

--- app/actions/getOverviewData7702.tsx
+++ app/actions/getOverviewData7702.tsx
@@ -14,7 +14,7 @@ interface ChainData {
 }
 
 export async function getOverviewData7702({ chain, timeframe }: OverviewDataParams): Promise<ChainData> {
-    const response = await fetch(`https://bundlebear-api.onrender.com/eip7702-overview?chain=${chain}&timeframe=${timeframe}`, { next: { revalidate: 3600 } });
+    const response = await fetch(`https://bundlebear-api.onrender.com/eip7702-overview?chain=${chain}&timeframe=${timeframe}`);
     // , { next: { revalidate: 3600 } }
 
     if (!response.ok) {
--- components/area-config.tsx
+++ components/area-config.tsx
@@ -9,6 +9,6 @@ export const CHAIN_AREAS_7702: BarConfig[] = [
     { dataKey: "bsc", fill: "#F0B90B", stroke: "#F0B90B" },
     { dataKey: "base", fill: "#90C2E7", stroke: "#90C2E7" },
     { dataKey: "optimism", fill: "#D1345B", stroke: "#D1345B" },
-    { dataKey: "gnosis", fill: "#0d8f75", stroke: "#0d8f75" },
-    { dataKey: "cross-chain", fill: "#823038", stroke: "#823038" }
+    { dataKey: "gnosis", fill: "#0d8f75", stroke: "#0d8f75" }
+    // { dataKey: "cross-chain", fill: "#823038", stroke: "#823038" }
 ];
