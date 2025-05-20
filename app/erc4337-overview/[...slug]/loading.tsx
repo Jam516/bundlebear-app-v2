52f99e598a49bf4d3f0136841b1a8bea93494914
Jam516 <kufuorkofi@gmail.com>
2025-05-20 16:27:40 +0100

    commit

--- app/erc4337-overview/[...slug]/loading.tsx
+++ app/erc4337-overview/[...slug]/loading.tsx
@@ -12,7 +12,8 @@ export default function Loading() {
                 <div className="flex items-center justify-between">
                     <h2 className="text-3xl font-bold tracking-tight">ERC-4337 Metrics</h2>
                 </div>
-                <div className="grid gap-4 md:grid-cols-3">
+                <div className="grid gap-4 md:grid-cols-4">
+                    <Skeleton className="h-[125px]" />
                     <Skeleton className="h-[125px]" />
                     <Skeleton className="h-[125px]" />
                     <Skeleton className="h-[125px]" />
