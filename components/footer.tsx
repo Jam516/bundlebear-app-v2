7381f61aaa85023fcc303eab5f7b60322ae96313
Jam516 <kufuorkofi@gmail.com>
2025-06-10 14:01:14 +0100

    commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,27 +2,39 @@ export function SiteFooter() {
     return (
         <footer className="border-t border-gray-300">
             <div className="mt-3 hidden md:flex flex-row justify-between">
-                <p className="text-center text-sm text-muted-foreground">
-                    Built by{" "}
-                    <a
-                        href="https://twitter.com/0xKofi"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="font-medium underline underline-offset-4"
-                    >
-                        0xKofi
-                    </a>
-                    . The source code is available on{" "}
-                    <a
-                        href="https://github.com/Jam516/BundleBear/tree/main"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="font-medium underline underline-offset-4"
-                    >
-                        GitHub
-                    </a>
-                    .
-                </p>
+                <div className="flex flex-col gap-2">
+                    <p className="text-center text-sm text-muted-foreground">
+                        Built by{" "}
+                        <a
+                            href="https://twitter.com/0xKofi"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="font-medium underline underline-offset-4"
+                        >
+                            0xKofi
+                        </a>
+                        . The source code is available on{" "}
+                        <a
+                            href="https://github.com/Jam516/BundleBear/tree/main"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="font-medium underline underline-offset-4"
+                        >
+                            GitHub
+                        </a>
+                        .
+                    </p>
+                    <div className=" flex flex-row gap-2 text-center text-sm text-muted-foreground">
+                        Powered by{" "}
+                        <a
+                            href="https://www.allium.so/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <img src="/allium-logo.png" className="h-4" alt="Allium" />
+                        </a>
+                    </div>
+                </div>
                 <div className=" flex flex-row gap-3 text-center text-sm text-muted-foreground">
                     Sponsored by{" "}
                     <a
@@ -70,6 +82,16 @@ export function SiteFooter() {
                     </a>
                     .
                 </p>
+                <div className=" flex flex-row gap-2 text-center text-sm text-muted-foreground">
+                    Powered by{" "}
+                    <a
+                        href="https://www.allium.so/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src="/allium-logo.png" className="h-5" alt="Allium" />
+                    </a>
+                </div>
                 <div className=" flex flex-row gap-3 text-center text-sm text-muted-foreground">
                     Sponsored by{" "}
                     <a
